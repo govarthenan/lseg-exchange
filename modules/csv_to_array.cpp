71c391fabf14d2cec9e5cdaa088b79b57d54b8e6
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-05-27 09:51:52 +0530

    Add comments

--- modules/csv_to_array.cpp
+++ modules/csv_to_array.cpp
@@ -10,17 +10,17 @@ int main() {
     string row[5];  // To store each row
     string word;
 
-    if (!data_file) {
+    if (!data_file) {  // Check if file is read
         cout << "Error reading file!" << endl;
         return 0;
     } else {
         cout << "File read successfully!" << endl;
     }
 
-    while (data_file.good()) {
+    while (data_file.good()) {  // When filestream is being read, iterate each line
         for (int i = 0; i < 4; i++) {
-            getline(data_file, row[i], ',');
+            getline(data_file, row[i], ',');  // Read each value up to comma and store it inside array
         }
-        getline(data_file, row[4], '\n');
+        getline(data_file, row[4], '\n');  // For the last value, read till newline
     }
 }
