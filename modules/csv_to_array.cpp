82bf42b2575df978fafa5c1c0767701d49685c3a
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-05-27 11:58:17 +0530

    Append each row's array with seperate values to master vector

--- modules/csv_to_array.cpp
+++ modules/csv_to_array.cpp
@@ -1,14 +1,16 @@
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
     ifstream data_file(".\\orders.csv");  // Orders source file
 
-    string row[5];  // To store each row
-    string word;
+    array<string, 5> row;             // To store each row
+    vector<array<string, 5>> orders;  // Master vector to store transaction orders
 
     if (!data_file) {  // Check if file is read
         cout << "Error reading file!" << endl;
@@ -22,5 +24,15 @@ int main() {
             getline(data_file, row[i], ',');  // Read each value up to comma and store it inside array
         }
         getline(data_file, row[4], '\n');  // For the last value, read till newline
+
+        orders.push_back(row);  // Append curren't row's array to the vector
     }
+
+    // DEBUG: To print read values
+    // for (auto row : orders) {
+    //     for (auto data : row) {
+    //         cout << data << " ";
+    //     }
+    //     cout << endl;
+    // }
 }
